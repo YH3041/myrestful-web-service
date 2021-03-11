8c551378000c245b1aa3757dd12b00deb70bb1d2
YH3041 <mmj2rv9@gmail.com>
2021-03-12 00:05:14 +0900

    Initial commit

---
+++ src/main/java/com/example/myrestfulwebservice/MyrestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyrestfulWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myrestfulwebservice/MyrestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
